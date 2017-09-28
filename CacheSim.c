a8a8e4b28ec5ade87f2a20d1a8c3ba85bb83d90c
Unknown <jmuri@umass.edu>
2017-09-27 22:43:13 -0400

    I have osteoperosis

--- CacheSim.c
+++ CacheSim.c
@@ -70,7 +70,6 @@ int offsetLength(int k, int l, int c){
 
 
 int tagBits(unsigned int address, int offset, int setIndex){	
-	//hairyPeanuses
 	assert((offset + setIndex) < 32); 
 	address = address >> (offset + setIndex);
 	return address;
